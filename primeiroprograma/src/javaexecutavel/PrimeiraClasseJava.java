c11e002c24ac89fa84c332d34a27e7583a47161d
Luciano <Luciano@DESKTOP-PE1CP5M>
2024-05-21 22:24:21 -0300

    criação do projeto.

---
+++ primeiroprograma/src/javaexecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,7 @@
+package javaexecutavel;
+
+public class PrimeiraClasseJava {
+public static void main(String[] args) {
+	System.out.println("Meu primeiro programa em java!! do curso formação javaweb");
+}
+}
